8ed344e6cd96e1c13d3d031263388f0cd8e95472
phirook223 <rascalcamel@naver.com>
2015-03-25 16:28:24 +0900

    Create a repository ‘ewha-ssp-hw1-2’
    Create a repository ‘ewha-ssp-hw1-2’ by using the GitHub Client

---
+++ ssp_lab2_1.cpp
@@ -0,0 +1,49 @@
+// Do not remove the include below
+#include "ssp_lab2_1.h"
+
+//The setup function is called once at startup of the sketch
+
+#define REAR_LED_PIN 9
+#define DUTY_CYCLE 20
+
+int percent = 100;
+
+void PWM_Write(int pin, int on_time_perc){
+	int on_time = (DUTY_CYCLE/100.0)*percent;
+	int off_time =DUTY_CYCLE-on_time;
+
+	digitalWrite(pin,HIGH);
+	delay(on_time);
+
+	digitalWrite(pin,LOW);
+	delay(off_time);
+}
+void myAnalogWrite(int pin, int percent, int time){
+
+
+	for(int i=0;i<time;i+=DUTY_CYCLE){
+		PWM_Write(pin,percent);
+	}
+
+
+}
+
+void setup()
+{
+// Add your initialization code here
+	pinMode(REAR_LED_PIN,OUTPUT);
+}
+
+// The loop function is called in an endless loop
+void loop()
+{
+//Add your repeated code here
+	int time = 2000;
+
+	myAnalogWrite(REAR_LED_PIN,percent,time);
+
+	if (percent!=0){
+		percent -=25;
+	}
+
+}
---
+++ ssp_lab2_1.h
@@ -0,0 +1,29 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef ssp_lab2_1_H_
+#define ssp_lab2_1_H_
+#include "Arduino.h"
+//add your includes for the project ssp_lab2 here
+
+
+//end of add your includes here
+#ifdef __cplusplus
+extern "C" {
+#endif
+void loop();
+void setup();
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+//add your function definitions for the project ssp_lab2 here
+
+
+
+
+//Do not add code below this line
+#endif /* ssp_lab2_H_ */
